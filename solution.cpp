be8a053789358812d4cfd209af94d8896b8db853
Steven Lin <steven_lx@outlook.com>
2022-06-19 11:20:32 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -26,11 +26,13 @@ using namespace std;
     so that's why we set dp[0] to 1
   - {} -> {1}, there is ony one way
 
-- next target = 2, scan the entire set to calculate dp[2].
+- next target = 2, scan the entire set {1,2,3} to calculate dp[2].
+  - dp[2] = dp[2-1] + dp[2-2] + dp[2-3]
+          = dp[1] + dp[0] (dp[2-3] is ignored)
   - There are two possibilities:
     - start from 0 and add a 2 {2}
     - start from 1 and add another 1 {1,1}
-    - so dp[2] = dp[0] + dp[1] = 1 + 1 = 2
+    - so dp[2] = dp[0(2-2)] + dp[1(2-1)] = 1 + 1 = 2
 - next target = 3, scan the entire set to calculate dp[3].
   - start from 0 and add a 3  {} ->  {3}
   - start from 1 and add a 2   {1} -> {1,2}
@@ -43,8 +45,8 @@ using namespace std;
   - start from 0 won't work
     - you can come to 4 from 1 by adding a '3', from
       2 by adding a '2', and from '3' by adding a '1'.
-    - since you only have {1,2,3} to choose from 
-      so you can't start from a '0' 
+    - since you only have {1,2,3} to choose from
+      so you can't start from a '0'
 
   - start from 1 you can add a 3, so {1} -> {1,3}
   - start from 2 you can add a 2, so {2} -> {2,2} and {1,1} -> {1,1,2}
@@ -63,10 +65,8 @@ int Solution::find(vector<int> &nums, int target)
   for (auto t = 1; t <= target; t++)
     for (auto num : nums)
       if (t - num >= 0)
-        /* each combination that has its members summing up to t-num
-           can be expended by adding "num" as its new member so
-           the expended combination will have its members summing up to
-           t
+        /* every sequence that has its members summing up to t-num
+           can be expended by adding "num" to get a sum of t
         */
         dp[t] += dp[t - num];
 
